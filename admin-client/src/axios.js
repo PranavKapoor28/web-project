1c0bfcf41ca439d0642b5e2274b9907a2f294025
jainam240101 <jainamm47@gmail.com>
2022-12-08 17:35:53 -0500

    Port Change

--- admin-client/src/axios.js
+++ admin-client/src/axios.js
@@ -1,8 +1,8 @@
-import axios from 'axios';
+import axios from "axios";
 
 //axios instance to base url
 const instance = axios.create({
-  baseURL: "http://localhost:5001/api/admins",
+  baseURL: "http://localhost:5003/api/admins",
 });
 
 export default instance;

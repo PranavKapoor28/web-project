f8b34110129d60d1d45a2cadc5f88e1d6b247a0a
Pranav Kapoor <pranavkapoor2k@gmail.com>
2022-12-08 03:18:25 -0500

    Made about page responsive and improved font and spacing

--- client/src/About/a.jsx
+++ client/src/About/a.jsx
@@ -14,22 +14,6 @@ const About = (props) => {
       <div className='jumbotron text-center'>
         <h1>Car-O-Pedia</h1>
         <p>Where renting gets easy !</p>
-        <form className='form-inline'>
-          <div className='input-group'>
-            <input
-              type='email'
-              className='form-control'
-              size='50'
-              placeholder='Email Address'
-              required
-            />
-            <div className='input-group-btn'>
-              <button type='button' className='btn btn-danger'>
-                Subscribe
-              </button>
-            </div>
-          </div>
-        </form>
       </div>
       <div className='container-fluid'>
         <div className='row'>
@@ -44,7 +28,6 @@ const About = (props) => {
               We offer market's best rates and offer full insurance for your
               trip, to make you worry-less and free.
             </p>
-            <button className='btn btn-default btn-lg'>Start booking</button>
           </div>
           <div className='col-sm-4'>
             <i
@@ -64,7 +47,7 @@ const About = (props) => {
           <div className='col-sm-4'>
             <span className='glyphicon glyphicon-globe logo'></span>
           </div>
-          <div className='col-sm-8'>
+          <div className='values col-sm-8'>
             <h2>Our Values</h2>
             <h4>
               <strong>MISSION:</strong> To offer reliable services and make
@@ -79,42 +62,44 @@ const About = (props) => {
 
       <div className='Team text-center'>
         <h1>Our Team</h1>
-        <div className='col-md-2'>
-          <img
-            className='img-fluid img-thumbnail'
-            src='https://media-exp1.licdn.com/dms/image/D4D35AQEUeWbE8ZTYQQ/profile-framedphoto-shrink_800_800/0/1663731945286?e=1670968800&v=beta&t=bTK1X5VthLC6MIgna4eeIZEalyBy-G2rdYf4efXtO8g'
-            alt=''
-          />
-        </div>
-        <div className='col-md-2'>
-          <img
-            className='img-fluid img-thumbnail'
-            src='https://avatars.githubusercontent.com/u/50455396?v=4'
-            alt=''
-          />
-        </div>
-        <div className='col-md-2'>
-          <img
-            className='img-fluid img-thumbnail'
-            src='https://media-exp1.licdn.com/dms/image/D4E03AQFth6JTfajRng/profile-displayphoto-shrink_400_400/0/1667513271397?e=1675900800&v=beta&t=ExdIqO_He7nxhcm5fMGj6ihE_bNrnJUJbg4gINNsjcM'
-            alt=''
-          />
-        </div>
-        <div className='col-md-2'>
-          <img
-            className='img-fluid img-thumbnail'
-            src='https://media-exp1.licdn.com/dms/image/D4E35AQFMHRHT7Ve-WQ/profile-framedphoto-shrink_400_400/0/1667868498208?e=1670968800&v=beta&t=PmCWG_wSYXELf_Nd63_S6M8Ara9Ln230VPKHOcCMWt0'
-            width='500'
-            height='600'
-            alt=''
-          />
-        </div>
-        <div className='col-md-2'>
-          <img
-            className='img-fluid img-thumbnail'
-            src='https://media-exp1.licdn.com/dms/image/D4E03AQFwGEVKr77l8Q/profile-displayphoto-shrink_800_800/0/1667520049982?e=1675900800&v=beta&t=fbcyXtcmk63j-tZxdEK7TQWL6u60vYwNr8Urb0yrego'
-            alt=''
-          />
+        <div className='Team_Container'>
+          <div className='col-md-2'>
+            <img
+              className='img-fluid img-thumbnail'
+              src='https://media-exp1.licdn.com/dms/image/D4D35AQEUeWbE8ZTYQQ/profile-framedphoto-shrink_800_800/0/1663731945286?e=1670968800&v=beta&t=bTK1X5VthLC6MIgna4eeIZEalyBy-G2rdYf4efXtO8g'
+              alt=''
+            />
+          </div>
+          <div className='col-md-2'>
+            <img
+              className='img-fluid img-thumbnail'
+              src='https://avatars.githubusercontent.com/u/50455396?v=4'
+              alt=''
+            />
+          </div>
+          <div className='col-md-2'>
+            <img
+              className='img-fluid img-thumbnail'
+              src='https://media-exp1.licdn.com/dms/image/D4E03AQFth6JTfajRng/profile-displayphoto-shrink_400_400/0/1667513271397?e=1675900800&v=beta&t=ExdIqO_He7nxhcm5fMGj6ihE_bNrnJUJbg4gINNsjcM'
+              alt=''
+            />
+          </div>
+          <div className='col-md-2'>
+            <img
+              className='img-fluid img-thumbnail'
+              src='https://media-exp1.licdn.com/dms/image/D4E35AQFMHRHT7Ve-WQ/profile-framedphoto-shrink_400_400/0/1667868498208?e=1670968800&v=beta&t=PmCWG_wSYXELf_Nd63_S6M8Ara9Ln230VPKHOcCMWt0'
+              width='500'
+              height='600'
+              alt=''
+            />
+          </div>
+          <div className='col-md-2'>
+            <img
+              className='img-fluid img-thumbnail'
+              src='https://media-exp1.licdn.com/dms/image/D4E03AQFwGEVKr77l8Q/profile-displayphoto-shrink_800_800/0/1667520049982?e=1675900800&v=beta&t=fbcyXtcmk63j-tZxdEK7TQWL6u60vYwNr8Urb0yrego'
+              alt=''
+            />
+          </div>
         </div>
       </div>
     </div>
--- client/src/User/components/Login.jsx
+++ client/src/User/components/Login.jsx
@@ -63,7 +63,7 @@ const Login = (props) => {
             onChange={onHandleChange}
             required
           />
-          {errors.email && <p style={marginBottom:20px} className='errors'>{errors.email}</p>}
+          {errors.email && <p className='errors'>{errors.email}</p>}
 
           {ValidateEmail.test(email) && (
             <p className='checked'>

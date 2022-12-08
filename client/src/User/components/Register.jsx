3df33ba8f6f72ec3982c9cd996251d469c67c705
Paurush Batish <paurushbatishfbd@gmail.com>
2022-12-08 15:28:06 -0500

    registration changes

--- client/src/User/components/Register.jsx
+++ client/src/User/components/Register.jsx
@@ -1,13 +1,15 @@
-import React, { useState, useRef, useEffect, Fragment } from 'react'
+import React, { useState, useRef, useEffect, Fragment , useContext } from 'react'
 import { AiOutlineCheckCircle } from 'react-icons/ai'
 import gsap from 'gsap'
 
 import { ValidateEmail, ValidateInput } from '../../shared/utils/validations'
 import AuthButton from '../../shared/components/Buttons/AuthButton';
 import PasswordField from './PasswordField';
+import { ThemeContext, themes} from '../../shared/contexts/ThemeContext';
 
 const Register = (props) => {
-
+    const { currentTheme, toggleTheme } = useContext(ThemeContext);
+    
     let authForm = useRef(null); 
     const [ errors, setErrors ] = useState({});
     const [ registerUser, setRegisterUser ] = useState({
@@ -53,22 +55,22 @@ const Register = (props) => {
 
     return (
         <div className='auth-container' ref={el => ( authForm = el)}>
-            <h3>Registration</h3>
-            <p>Before using our app, you need to create an account</p>
+            <h3 style={{color: '#2F3C7E' }}>Registration</h3>
+            <p style={{color: '#2F3C7E' }}>Before using our app, you need to create an account</p>
             <form className='auth-form' onSubmit={onHandleSubmit}>
                 <div className='input-fied'>
                     <input type='text' name='name' placeholder='Name...'  
-                    value={name} onChange={onHandleChange} required/>
+                    value={name} onChange={onHandleChange} required style={{color: '#2F3C7E' }}/>
                     {errors.name && <p className='errors'>{errors.name}</p>}
                     {name.length >= 3 && <p className='checked'><AiOutlineCheckCircle /></p>}
                 </div>
                 <div className='input-fied'>
                     <input type="email" name='email' placeholder='E-mail...'
-                    value={email} onChange={onHandleChange} required/>        
+                    value={email} onChange={onHandleChange} required style={{color: '#2F3C7E' }}/>        
                     {errors.email && <p className='errors'>{errors.email}</p>}
                     {ValidateEmail.test(email) && <p className='checked'><AiOutlineCheckCircle /></p>}
                 </div>
-                <div className='input-fied'>
+                <div className='input-fied' style={{color: '#2F3C7E' }}>
                     <PasswordField onHandleChange={onHandleChange}
                                     value={password}
                                     errors={errors.password}
@@ -80,13 +82,13 @@ const Register = (props) => {
                     {errors.password2 && <p className='errors'>{errors.password2}</p>}
                     { password2.length >= 6 && <p className='checked'><AiOutlineCheckCircle /></p>}
                 </div>
-                <div className='input-fied'>
+                <div className='input-fied' style={{color: '#2F3C7E' }}>
                     {
                         props.isLoading 
                         ? <AuthButton value='Register'/>
                         :
                         <Fragment>
-                            <input type="submit" value='Register'/>
+                            <input type="submit" value='Register' style={{color: '#2F3C7E' }}/>
                             {errors.msg && <p className='errors'>{errors.msg}</p>}
                         </Fragment>
                     }
